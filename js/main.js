698f72c649fd4fc8fb4e9d3232183f7873a6dbaf
Aman <rumman.djuice1@gmail.com>
2019-04-27 22:19:02 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
     $(".header-area").sticky({topSpacing:0});
 
+    $(".smooth-menu").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+            var h='70'
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - h + 'px'
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+
+            });
+        } // End if
+    });
+
 
 });
