6629de861a9a7b7ac59034dce9fd50093245c37f
Aman <rumman.djuice1@gmail.com>
2019-04-28 02:39:38 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -4,25 +4,33 @@ $(document).ready(function(){
     $(".smooth-menu").on('click', function(event) {
 
         // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
 
-            // Store hash
-            var hash = this.hash;
-            var h='70'
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top - h + 'px'
-            }, 800, function(){
+        // Store hash
+        var hash = this.hash;
+        var h='70'
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - h + 'px'
+        }, 1000, function(){
 
-            });
-        } // End if
+            // Add hash (#) to URL when done scrolling (default click behavior)
+
+        });
+        // End if
     });
 
 
+    $("body").scrollspy({
+        target: '.navbar',
+        offset: 10
+    });
+
 });
+
+
+
